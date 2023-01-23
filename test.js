fd60498ed54d602c67efec8e8200b672021720c7
Douglas <mercurymuur86@yahoo.com>
2023-01-23 02:05:38 -0500

    Indeed

---
+++ test.js
@@ -0,0 +1,9 @@
+// The first alert
+alert ("Hello, World!");
+
+// The second alert
+alert("hi again");
+
+// Move the image from top to bottom
+// Make the image invisible 
+// Make the image visible
