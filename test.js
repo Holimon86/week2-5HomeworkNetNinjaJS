7f500ab2ae10fed07964e4be8a1d6929b2645ceb
Douglas <mercurymuur86@yahoo.com>
2023-01-23 02:40:55 -0500

    Indeed

--- test.js
+++ test.js
@@ -7,3 +7,103 @@ alert("hi again");
 // Move the image from top to bottom
 // Make the image invisible 
 // Make the image visible
+
+var myVariableDoug
+myVariableDoug = 10;
+10
+var myVar2 = "hello";
+myVar2
+"hello";
+myVar2 = "hello again";
+myVar2 = 5;
+5
+var customerAge = 32;
+customerAge;
+32
+var myVar3 = 40;
+
+var myVar = 5
+myVar
+5
+5 + 5
+10
+myVar + 10
+15
+myVar
+5
+myVar = myVar + 10
+15
+myVar
+15
+10 - 5
+5
+myVar - 3
+12
+myVar
+10
+10 * 10
+100
+20 / 10
+2
+myVar = myVar / 2
+5
+myVar
+5
+5 + 5
+10
+5 + "Hello"
+"5Hello"
+100 + "Hello"
+"100Hello"
+"Hello" + "Douglas"
+"HelloDouglas"
+"Hello" + " Douglas"
+"Hello Douglas"
+5 * "Hello"
+NaN
+
+var myVar = 10
+myVar = myVar + 5
+15
+myVar
+15
+myVar += 5
+20
+myVar
+20
+myVar -= 10 
+10
+myVar
+10
+myVar /= 2
+5
+myVar
+5
+myVar *= 5
+25
+myVar
+25
+myVar += 1
+26
+myVar ++
+26
+myVar
+27
+++myVar
+28
+
+var myVar = Douglas
+undefined
+document.write(myVar)
+undefined
+console.log(myvar)
+Douglas
+document.write(10)
+undefined
+console.log(10)
+undefined
+console.log(20 * 5)
+100
+undefined
+document.write(20 / 5)
+undefined
